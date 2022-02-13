223cde53d3a618492b3450962412290f1ff3dfd7
Cristiano <cristianodevsystem@gmail.com>
2022-02-13 17:50:23 -0300

    :rocket:  Trabalhando com rodas

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    // Lista todos usuários
+    @Get()
+    findAll() {
+        return 'Lista todos os usuários';
+    }
+
+    // Listando um usuário
+    @Get(':id')
+    findOne(@Param() params) {
+        return `Usuários #${params.id}`
+    }
+}
