7c7be70151abf6abca07226d10efe752fc954e78
Cristiano <cristianodevsystem@gmail.com>
2022-02-13 18:20:49 -0300

    :rocket:  HTTP Status Code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('user')
 export class UserController {
 
     // Lista todos usuários
     @Get()
-    findAll() {
-        return 'Lista todos os usuários';
+    findAll(@Res() response) {
+        return response.status(200).send('Lista todos os usuários');
     }
 
     // Listando um usuário
@@ -15,7 +16,9 @@ export class UserController {
         return `Usuários #${id}`
     }
 
+    // Criando usuário
     @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
     create(@Body() body) {
         return body;
     }
