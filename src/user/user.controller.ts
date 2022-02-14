30d8b43cbd887d43e2741a091c834b4946e3846f
Cristiano <cristianodevsystem@gmail.com>
2022-02-14 16:04:47 -0300

    :rocket: Requisições HTTP PATH e DELETE

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,46 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { response } from 'express';
 
 @Controller('user')
 export class UserController {
+  // Lista todos usuários
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).send('Lista todos os usuários');
+  }
 
-    // Lista todos usuários
-    @Get()
-    findAll(@Res() response) {
-        return response.status(200).send('Lista todos os usuários');
-    }
+  // Listando um usuário
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Usuários #${id}`;
+  }
 
-    // Listando um usuário
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `Usuários #${id}`
-    }
+  // Criando usuário
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return body;
+  }
 
-    // Criando usuário
-    @Post()
-    @HttpCode(HttpStatus.NO_CONTENT)
-    create(@Body() body) {
-        return body;
-    }
+    //   Autulizando usuário
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+      return `Atualizando o usuário #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+      return `Usuário #${id} removido com sucesso!`
+  }
 }
